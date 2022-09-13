7e752c49faaa727dbc9423673cce75f3734aec9e
David Perelman <dperelman3@gmail.com>
2022-09-13 14:42:33 +0300

    setup server/index.js basic routes

--- server/index.js
+++ server/index.js
@@ -2,8 +2,34 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const path = require('path');
+var ReactDOMServer = require('react-dom/server');
+const React = require('react');
+import App from '../src/App';
+
+app.use('/static', express.static(path.join(__dirname, '..', 'public')));
+
 app.get('/', (req, res) => {
-  res.send({ msg: 'msg' });
+  const component = ReactDOMServer.renderToString(<App />);
+  const html = `
+  <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>React App</title>
+</head>
+
+<body>
+    <div id="root">${component}</div>
+    <script src="./static/bundle.js"></script>
+</body>
+
+</html>`;
+
+  res.send(html);
 });
 
 app.listen(port, () => {
